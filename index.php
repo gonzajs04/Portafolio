1779c53536eadcb5f2c0a114f0b844a7d581301c
Gonzalo <hernandezgonzalo5845@gmail.com>
2024-02-13 19:29:58 -0300

    feat: se agrego funcionalidad _blank a los proyectos apra que no se abran en la misma pestaña

--- index.php
+++ index.php
@@ -33,6 +33,18 @@ $certificados = [
 ];
 
 $proyectos = [
+
+    (new Proyecto("Hupa Hamburguesas", "hupa", "Diseño y desarrollo web de Hupa Hamburguesas. Esta pagina fue desarrollada con React, Vite, Sass, JavaScript. Se utilizo POSTCSS para minificacion de css y Terser para minificacion de JavaScript. Se utilizo TinyPng para comprimir tamaño de las imagenes.Se utilizo Lazy Load para cargar correctamente la pagina y sus imagenes. Este diseño es original y creado por mi del emprendimiento HUPA HAMBURGUESAS que se encuentra en Argentina - Buenos Aires ", "https://hupahamburguesas.netlify.app/")),
+
+
+        (new Proyecto("Cotizador de criptos", "cotizador", "Cotiza tus criptos en cualquier momento! Esta página fue desarrollada con REACT, y como terminal de ejecucion Vite. Se utilizo la libreria de Styled-components para estilizar componentes reutilizables. Asimismo, se consumió la API de CryptoCompare para extraer los datos de las mismas.", "https://cotizacriptom.netlify.app/")),
+    
+        (new Proyecto("PlayIt", "playit", "PlayIT esta desarrollado con RemixRun, el nuevo framework de React. Este fue utilizado para manejo de rutas, uso de loader para cargas de APIS desde el servidor, etiquetas meta para identificar en que ruta se encuentra el usuario. Para la estilización, se utilizó CSS puro. Además, se utilizo la API de greenborn para generar palabras aleatorias", "https://playit-gonzajs04.vercel.app/")),
+    
+        (new Proyecto("CloudMovie", "cloudmovie", "Este es mi primer proyecto como FULL STACK DEVELOPER PHP, el cual consiste, del manejo de las tecnologias: HTML,CSS,SCSS,JAVASCRIPT,GULP,PHP,
+        MYSQL. Este proyecto es una pagina de peliculas con mi propia base de datos, la cual podes, BUSCAR PELICULAS, VER SUS TRAILERS, FILTRAR POR GENEROS, Y POSEE UN ADMINISTRADOR DE PELICULAS PARA: Agregar peliculas, Actualizarlas y eliminarlas ", "https://cloudmoviegh.000webhostapp.com/")),
+        (new Proyecto("WeatherLike", "weatherlike", "Este es un proyecto el cual consumo el API de OPENWEATHER. Este proyecto permite visualizar todas las TEMPERATURAS y otras cosas de todo el mundo. Para ver la pagina, toca el link ", "https://weatherlikev2.netlify.app/")),
+    
     // (new Proyecto("Almas fortalecidas piedras", "PROYECTO2", "Este es mi primer proyecto de todos, en el cual solamente utilice HTML y CSS | This is my first project. I used only HTML and CSS", "https://almaspiedrasfortalecidas.netlify.app/")),
 
     // (new Proyecto("El Granizado", "Proyecto1", "Este es mi segundo proyecto. Ya con mas practica, empece a jugar un poco con HTML y CSS. | This is my second project, with more practice, I started to play a little bit with HTML and CSS", "https://elgranizado.netlify.app/")),
@@ -48,17 +60,6 @@ $proyectos = [
 
     // ", "https://bienesraicesfront.netlify.app/")),
 
-
-    (new Proyecto("Hupa Hamburguesas", "hupa", "Diseño y desarrollo web de Hupa Hamburguesas. Esta pagina fue desarrollada con React, Vite, Sass, JavaScript. Se utilizo POSTCSS para minificacion de css y Terser para minificacion de JavaScript. Se utilizo TinyPng para comprimir tamaño de las imagenes.Se utilizo Lazy Load para cargar correctamente la pagina y sus imagenes. Este diseño es original y creado por mi del emprendimiento HUPA HAMBURGUESAS que se encuentra en Argentina - Buenos Aires ", "https://hupahamburguesas.netlify.app/")),
-
-
-    (new Proyecto("Cotizador de criptos", "cotizador", "Cotiza tus criptos en cualquier momento! Esta página fue desarrollada con REACT, y como terminal de ejecucion Vite. Se utilizo la libreria de Styled-components para estilizar componentes reutilizables. Asimismo, se consumió la API de CryptoCompare para extraer los datos de las mismas.", "https://cotizacriptom.netlify.app/")),
-
-    (new Proyecto("PlayIt", "playit", "PlayIT esta desarrollado con RemixRun, el nuevo framework de React. Este fue utilizado para manejo de rutas, uso de loader para cargas de APIS desde el servidor, etiquetas meta para identificar en que ruta se encuentra el usuario. Para la estilización, se utilizó CSS puro. Además, se utilizo la API de greenborn para generar palabras aleatorias", "https://playit-gonzajs04.vercel.app/")),
-
-    (new Proyecto("CloudMovie", "cloudmovie", "Este es mi primer proyecto como FULL STACK DEVELOPER PHP, el cual consiste, del manejo de las tecnologias: HTML,CSS,SCSS,JAVASCRIPT,GULP,PHP,
-    MYSQL. Este proyecto es una pagina de peliculas con mi propia base de datos, la cual podes, BUSCAR PELICULAS, VER SUS TRAILERS, FILTRAR POR GENEROS, Y POSEE UN ADMINISTRADOR DE PELICULAS PARA: Agregar peliculas, Actualizarlas y eliminarlas ", "https://cloudmoviegh.000webhostapp.com/")),
-    (new Proyecto("WeatherLike", "weatherlike", "Este es un proyecto el cual consumo el API de OPENWEATHER. Este proyecto permite visualizar todas las TEMPERATURAS y otras cosas de todo el mundo. Para ver la pagina, toca el link ", "https://weatherlikev2.netlify.app/")),
 
     (new Proyecto("Veterinario", "perro", "Veterinario es un Administrador de pacientes. Es mi primer proyecto hecho con REACT, TAILWIND CSS, VITE y LocalStorage. Puedes registrar los sintomas de tu mascota, editarlos o eliminar el sintoma. Puedes encontrar la pagina haciendo click ", "https://administradormascotasvite.netlify.app/")),
     (new Proyecto("BlueeBerry", "blueberry", "Blueeberry es una plataforma que ofrece las mejores joyas de Inglaterra. Desarrollada con React, Sass y Vite, brinda una experiencia visual atractiva y funcional. La integración de  permite una navegación fluida entre secciones. Descubre nuestras exquisitas joyas en un solo lugar. ¡Bienvenido a Blueberry! ", "https://blueberryj.netlify.app/")),
@@ -160,7 +161,7 @@ $proyectos = [
                 <!--PROYECTO 1-->
 
                 <div class="proyecto swiper-slide">
-                    <a href="<?php echo $proyecto->getUrl() ?>">
+                    <a href="<?php echo $proyecto->getUrl() ?>" target="_blank">
 
                         <div class="img-proyecto">
                             <picture>
